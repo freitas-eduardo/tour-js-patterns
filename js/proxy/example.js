bdb32bc4c5606c58d74e86eeba04adb0f33c3dc0
Eduardo Freitas <freitas.eduardo.gon@gmail.com>
2023-03-20 09:05:45 -0300

    observers + proxy

---
+++ js/proxy/example.js
@@ -0,0 +1,19 @@
+const person = {
+    name: "John Doe",
+    age: 42,
+    email: "john@doe.com",
+    country: "Canada",
+  };
+  
+  const personProxy = new Proxy(person, {
+    get: (target, prop) => {
+      console.log(`The value of ${prop} is ${target[prop]}`);
+      return target[prop];
+    },
+    set: (target, prop, value) => {
+      console.log(`Changed ${prop} from ${target[prop]} to ${value}`);
+      target[prop] = value;
+      return true;
+    },
+  });
+
